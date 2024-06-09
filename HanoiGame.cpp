eb3c7262630aff987f0d8a7e899ac0b882d3f5ff
SeongJun08 <ellajune1612@gmail.com>
2024-06-09 23:14:54 +0900

    배열 선언 및 출력

--- HanoiGame.cpp
+++ HanoiGame.cpp
@@ -2,5 +2,32 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int array[3][3]
+    {
+        { 1,0,0 },
+        { 2,0,0 },
+        { 3,0,0 }
+    };
+
+    int layer, layer2;
+
+    while (true)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                std::cout << array[i][j] << "  ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "어떻게 움직일까요? ";
+        std::cin >> layer >> layer2;
+
+        if (layer == 1)
+        {
+
+        }
+    }
+
 }
